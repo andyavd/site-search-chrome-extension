2daa25f6ba8ab66e154fd0a5b92155303383e98e
andyavd <andyavd@yandex.ru>
2017-08-24 17:23:44 +0300

    Add files via upload

---
+++ popup.js
@@ -0,0 +1,12 @@
+var elem = document.getElementById("searchform");
+elem.addEventListener("submit", function() {
+    var value1 = document.getElementById('entertext').value;
+
+    var value2 = document.getElementById('site').value;
+
+
+    var value3 = value1 + value2;
+
+	
+    document.getElementById('finalquery').setAttribute('value', value3);
+},false);
